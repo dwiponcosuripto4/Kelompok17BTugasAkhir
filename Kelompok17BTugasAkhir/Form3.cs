f14138731b9ee8d53b7f2a2cdc3b85d07c331401
Dwiponco Suripto <d.suripto.ft21@mail.umy.ac.id>
2023-07-09 22:59:28 +0700

    koneksi form 2 3 4

--- Kelompok17BTugasAkhir/Form3.cs
+++ Kelompok17BTugasAkhir/Form3.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ namespace Kelompok17BTugasAkhir
 {
     public partial class Form3 : Form
     {
+        private string stringConnection = "data source=LAPTOP-9OD41I73\\DWIPONCOS;database=Kos;User ID=sa; Password=xm11tpro";
+        private SqlConnection koneksi;
         public Form3()
         {
             InitializeComponent();
